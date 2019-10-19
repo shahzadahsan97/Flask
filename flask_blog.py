1f7ff7496fb01025b47483037d40663a4717ef7f
Shahzad Ahsan <dankara97@gmail.com>
2019-10-19 14:15:47 +0500

    added debug (dont have to restart page again...

--- flask_blog.py
+++ flask_blog.py
@@ -1,13 +1,14 @@
 from flask import Flask as f
 app = f(__name__)  #create instance
 
+# app.debug = True
 
 @app.route('/')  #create route otherwise it  will give error 404..
 def hello():
-    return 'Hello Flask'
+    return 'Hello Flask111'
 
 
 if __name__ == 'main':
-    app.run()
+    app.run(debug=True)   # if we add debug = true its mean that we dont have to refresh our page again n again...
 
     
