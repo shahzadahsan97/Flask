83157007a66d0612b9946fcc94e7396f3bc28852
Shahzad Ahsan <dankara97@gmail.com>
2019-11-18 14:08:42 +0500

    make aboutUs html file and make route to render templates

--- render_templetes.py
+++ render_templetes.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def hello():
     return render_template('home.html') #it goes to html file....
 
+@app.route('/about')
+def about():
+    return render_template('aboutt.html') #it goes to about.html file.....
+
+
 
 if __name__ == 'main':
     app.run(debug=True) 
